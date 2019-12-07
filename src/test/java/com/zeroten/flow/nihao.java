8878c867b5d914a840b441bf7c5249cf118a22be
daibaohui <1275614266@qq.com>
2019-12-07 12:20:22 +0800

    修改 nihao.class

--- src/test/java/com/zeroten/flow/nihao.java
+++ src/test/java/com/zeroten/flow/nihao.java
@@ -3,5 +3,11 @@ package com.zeroten.flow;
 public class nihao {
     public static void main(String[] ages){
         System.out.print("|nihao");
+        ma();
+    }
+  
+    public static void ma(){
+        System.out.println("nishi");
     }
 }
+
